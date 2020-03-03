c9990536f5ac78373fb82ccc3df626b8e6e7073f
MajidHws <majidhws@gmail.com>
2020-03-03 19:31:06 +0300

    2

--- functions.js
+++ functions.js
@@ -1,5 +1,20 @@
+const axios = require('axios')
 const functions = {
-    add: (num1, num2) => num1 + num2
+    add: (num1, num2) => num1 + num2,
+    isNull: () => null,
+    checkValue: (x) => x,
+    createUser: () => {
+        const user = { firstName: 'Majid' }
+        user['lastName'] = 'Howsawi'
+        return user
+    },
+    equality: (load1, load2) => load1 + load2,
+    checkString: (text) => text,
+    isContain: (user) => user,
+    fetchUser: () => axios
+    .get('https://jsonplaceholder.typicode.com/users/1')
+        .then(res => res.data)
+        .catch(e => '')
 }
 
 module.exports = functions
--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,66 @@
 const functions = require('./functions')
 
+// beforeAll(() => console.log('DB is connected...'))
+// afterAll(() => console.log('DB is closed...'))
+
+beforeEach(() => console.log('DB is connected...'))
+afterEach(() => console.log('DB is closed...'))
+
+describe('describe checking names', () => {
+    beforeEach(() => console.log('majid & majis...'))
+    test('checking names to be true', () => {
+        expect('Maji' + 'd').toBe('Majid')
+    })
+
+    test('checking names to be true', () => {
+        expect('Maji' + 's').toBe('Majis')
+    })
+})
+
+
+
 test('Addition Function', () => {
     expect(functions.add(2, 2)).toBe(4)
 })
 
 test('Addition using NOT', () => {
-    expect(functions.add(2, 2)).not.toBe(5)
+    expect(functions.add(2, 2)).not.toBe(5) // this only works with primitive like not arrays or objects
+})
+
+test('To be null', () => {
+    expect(functions.isNull()).toBeNull()
+})
+
+test('To be falsy', () => {
+    expect(functions.checkValue(undefined)).toBeFalsy()
+})
+
+test('return an object', () => {
+    expect(functions.createUser()).toEqual({ // this only works arrays or objects
+        firstName: 'Majid',
+        lastName: 'Howsawi'
+    })
+})
+
+test('To be less than', () => {
+    expect(functions.equality(800, 700)).toBeLessThan(1600)
+})
+
+test('check some characters in a string', () => {
+    // provide the letters you don't want to include in the regex and it is not case sensitive
+    expect(functions.checkString('Majid')).not.toMatch(/p/) 
+})
+
+test('check if user in the array', () => {
+    const usernames = ['Majid', 'Admin']
+    expect(usernames).toContain(functions.isContain('Majid'))
+})
+
+test('async data', () => {
+    expect.assertions(1)
+
+    return functions.fetchUser()
+    .then(res => {
+        expect(res.name).toEqual('Leanne Graham')
+    })
 })
